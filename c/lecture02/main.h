1fa8d2d29e757b7f39a6ac56d1fa55199682aa5d
Edwin Bermudes <74934197+redwinbee@users.noreply.github.com>
2023-05-04 07:39:14 -0400

    lecture02: complete

---
+++ c/lecture02/main.h
@@ -0,0 +1,14 @@
+//
+// Created by Edwin Bermudes on 5/2/23.
+//
+
+#ifndef LECTURE02_MAIN_H
+#define LECTURE02_MAIN_H
+
+int demo_pointer();
+int func_math(int x, int y, int(*func_p)(int, int));
+int add(int x, int y);
+int sub(int x, int y);
+void swap(int* a, int* b);
+
+#endif //LECTURE02_MAIN_H
